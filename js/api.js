19b19849913aa28e8b813c65ac36cc976dfebb1b
Jeff <jeffreyhuytrang@yahoo.com>
2020-03-06 13:36:18 -0800

    initial commit

---
+++ js/api.js
@@ -0,0 +1,31 @@
+$(() => {
+    $('#searchForm').on('submit', (e) => {
+        e.preventDefault();
+        let searchTermString = $('#searchText').val();
+        fetchListOfMovies(searchTermString);
+    });
+})
+
+let fetchListOfMovies = (searchTermString) => {
+    let baseURL = `http://www.omdbapi.com/?s=${searchTermString}&apikey=1efcc1a3`;
+    axios.get(baseURL).then((response) => {
+        let titleOfMoviesResponse = response.data.Search;
+        let html = '';
+
+        $.each(titleOfMoviesResponse, (index, movie) => {
+            html += `
+                <div class="col-md-3>
+                    <div class="well text-center>
+                        <img src="${movie.Poster}">
+                        <h5>${movie.Title}</h5>
+                    </div>
+                </div>
+            `
+        });
+
+        $('#movies').html(html);
+    }).catch((err) => {
+        console.log(err);
+    })
+    
+}
